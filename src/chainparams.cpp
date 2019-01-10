f93f897c08ef0f995aff84be7232c87f500afe99
bazarijoo <bitaazarijoo@gmail.com>
2019-01-10 04:11:25 -0500

    some logs added in miner.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -108,7 +108,7 @@ public:
 
         // The best chain should have at least this much work.
         // "0x0000000000000000000000000000000000000000028822fef1c230963535a90d"
-        consensus.nMinimumChainWork = uint256S("0000000000000000000000000000000000000000000000000000000000000101");
+        consensus.nMinimumChainWork = uint256S("0x0000000000000000000000000000000000000000000000000000000000000101");
 
         // By default assume that the signatures in ancestors of this block are valid.
         consensus.defaultAssumeValid = uint256S("0x0000000000000000002e63058c023a9a1de233554f28c7b21380b6c9003f36a8"); //534292
